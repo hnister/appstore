3e4f935c4348d1f086e1b9190356478ea691ad17
grizzly <wxfgrizzly@qq.com>
2019-12-31 09:26:49 +0800

    添加Controller层

--- src/com/grizzly/controller/ManagerController.java
+++ src/com/grizzly/controller/ManagerController.java
@@ -1,23 +1,35 @@
 package com.grizzly.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.grizzly.model.AppCategory;
+import com.grizzly.model.AppInfo;
+import com.grizzly.model.AppVersion;
 import com.grizzly.model.BackendUser;
+import com.grizzly.model.DataDictionary;
+import com.grizzly.service.DeveloperService;
 import com.grizzly.service.ManagerService;
 import com.grizzly.utils.Constants;
+import com.grizzly.utils.PageSupport;
 
 @Controller
 public class ManagerController {
 	
-	@Autowired
-	private ManagerService managerService;
+    @Autowired
+    private DeveloperService developerService;
+    @Autowired
+    private ManagerService managerService;
 	
 	@RequestMapping("manager/login")
 	public String toManagerLoginPage() {
@@ -60,5 +72,176 @@ public class ManagerController {
 		//清除session
 		session.removeAttribute(Constants.USER_SESSION);
 		return "backendlogin";
+	}
+	
+	@RequestMapping(value="manager/backend/app/list")
+	public String getAppInfoList(Model model,HttpSession session,
+							@RequestParam(value="querySoftwareName",required=false) String querySoftwareName,
+							@RequestParam(value="queryCategoryLevel1",required=false) String _queryCategoryLevel1,
+							@RequestParam(value="queryCategoryLevel2",required=false) String _queryCategoryLevel2,
+							@RequestParam(value="queryCategoryLevel3",required=false) String _queryCategoryLevel3,
+							@RequestParam(value="queryFlatformId",required=false) String _queryFlatformId,
+							@RequestParam(value="pageIndex",required=false) String pageIndex){
+		
+
+		
+		List<AppInfo> appInfoList = null;
+		List<DataDictionary> flatFormList = null;
+		List<AppCategory> categoryLevel1List = null;//列出一级分类列表，注：二级和三级分类列表通过异步ajax获取
+		List<AppCategory> categoryLevel2List = null;
+		List<AppCategory> categoryLevel3List = null;
+		//页面容量
+		int pageSize = Constants.pageSize;
+		//当前页码
+		Integer currentPageNo = 1;
+		
+		if(pageIndex != null){
+			try{
+				currentPageNo = Integer.valueOf(pageIndex);
+			}catch (NumberFormatException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		Integer queryCategoryLevel1 = null;
+		if(_queryCategoryLevel1 != null && !_queryCategoryLevel1.equals("")){
+			queryCategoryLevel1 = Integer.parseInt(_queryCategoryLevel1);
+		}
+		Integer queryCategoryLevel2 = null;
+		if(_queryCategoryLevel2 != null && !_queryCategoryLevel2.equals("")){
+			queryCategoryLevel2 = Integer.parseInt(_queryCategoryLevel2);
+		}
+		Integer queryCategoryLevel3 = null;
+		if(_queryCategoryLevel3 != null && !_queryCategoryLevel3.equals("")){
+			queryCategoryLevel3 = Integer.parseInt(_queryCategoryLevel3);
+		}
+		Integer queryFlatformId = null;
+		if(_queryFlatformId != null && !_queryFlatformId.equals("")){
+			queryFlatformId = Integer.parseInt(_queryFlatformId);
+		}
+		
+		//总数量（表）
+		int totalCount = 0;
+		try {
+			totalCount = managerService.getAppInfoCount(querySoftwareName, queryCategoryLevel1, queryCategoryLevel2, queryCategoryLevel3, queryFlatformId);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//总页数
+		PageSupport pages = new PageSupport();
+		pages.setCurrentPageNo(currentPageNo);
+		pages.setPageSize(pageSize);
+		pages.setTotalCount(totalCount);
+		int totalPageCount = pages.getTotalPageCount();
+		//控制首页和尾页
+		if(currentPageNo < 1){
+			currentPageNo = 1;
+		}else if(currentPageNo > totalPageCount){
+			currentPageNo = totalPageCount;
+		}
+		try {
+			appInfoList = managerService.getAppInfoList(querySoftwareName, queryCategoryLevel1, queryCategoryLevel2, queryCategoryLevel3, queryFlatformId, currentPageNo, pageSize);
+			flatFormList = this.getDataDictionaryList("APP_FLATFORM");
+			
+			//*
+			categoryLevel1List = developerService.getAppCategoryListByParentId(null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("appInfoList", appInfoList);
+		model.addAttribute("flatFormList", flatFormList);
+		model.addAttribute("categoryLevel1List", categoryLevel1List);
+		model.addAttribute("pages", pages);
+		model.addAttribute("querySoftwareName", querySoftwareName);
+		model.addAttribute("queryCategoryLevel1", queryCategoryLevel1);
+		model.addAttribute("queryCategoryLevel2", queryCategoryLevel2);
+		model.addAttribute("queryCategoryLevel3", queryCategoryLevel3);
+		model.addAttribute("queryFlatformId", queryFlatformId);
+		
+		//二级分类列表和三级分类列表---回显
+		if(queryCategoryLevel2 != null && !queryCategoryLevel2.equals("")){
+			categoryLevel2List = getCategoryList(queryCategoryLevel1.toString());
+			model.addAttribute("categoryLevel2List", categoryLevel2List);
+		}
+		if(queryCategoryLevel3 != null && !queryCategoryLevel3.equals("")){
+			categoryLevel3List = getCategoryList(queryCategoryLevel2.toString());
+			model.addAttribute("categoryLevel3List", categoryLevel3List);
+		}
+		return "backend/applist";
+	}
+	
+	public List<DataDictionary> getDataDictionaryList(String typeCode){
+		List<DataDictionary> dataDictionaryList = null;
+		try {
+			dataDictionaryList = developerService.getDataDictionaryList(typeCode);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dataDictionaryList;
+	}
+	
+	public List<AppCategory> getCategoryList (String pid){
+		List<AppCategory> categoryLevelList = null;
+		try {
+			categoryLevelList = developerService.getAppCategoryListByParentId(pid==null?null:Integer.parseInt(pid));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return categoryLevelList;
+	}
+	
+	/**
+	 * 根据parentId查询出相应的分类级别列表
+	 * @param pid
+	 * @return
+	 */
+	@RequestMapping(value="manager/backend/app/categorylevellist.json",method=RequestMethod.GET)
+	@ResponseBody
+	public List<AppCategory> getAppCategoryList (@RequestParam String pid){
+		if(pid.equals("")) pid = null;
+		return getCategoryList(pid);
+	}
+	
+	
+	/**
+	 * 跳转到APP信息审核页面
+	 * @param appId
+	 * @param versionId
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="manager/backend/app/check",method=RequestMethod.GET)
+	public String check(@RequestParam(value="aid",required=false) String appId,
+					   @RequestParam(value="vid",required=false) String versionId,
+					   Model model){
+		AppInfo appInfo = null;
+		AppVersion appVersion = null;
+		try {
+			appInfo = managerService.getAppInfo(Integer.parseInt(appId));
+			appVersion = developerService.getAppVersionById(Integer.parseInt(versionId));
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute(appVersion);
+		model.addAttribute(appInfo);
+		return "backend/appcheck";
+	}
+	@RequestMapping(value="manager/backend/app/checksave",method=RequestMethod.POST)
+	public String checkSave(AppInfo appInfo){
+		try {
+			if(managerService.updateSatus(appInfo.getStatus(),appInfo.getId())){
+				return "redirect:/manager/backend/app/list";
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "backend/appcheck";
 	}
 }
